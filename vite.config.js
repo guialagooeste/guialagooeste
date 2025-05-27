d2f414cb9e3aa8e849ebc316b5ddaddf16d4acd5
guialagooeste <contato@guialagooeste.com.br>
2025-05-26 23:36:06 -0300

    Configura Vite para usar 'public' como root e define outDir

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Usa a porta padrão do Vite (5173).
-    // Se ela estiver ocupada, o Vite tentará a próxima disponível.
-    port: 5173
+  root: 'public', // <--- Adicione esta linha!
+  build: {
+    outDir: 'dist', // Adicione esta linha também, para garantir que o build vá para a pasta 'dist' na raiz
+    emptyOutDir: true, // Adicione esta linha para limpar a pasta 'dist' antes de cada build
   }
 });
